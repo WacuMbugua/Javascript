ba7fa2a82f927d2b7a99232686bc0a7243c9d1b2
WacuMbugua <58767172+WacuMbugua@users.noreply.github.com>
2019-12-27 04:41:43 +0300

    Add files via upload
    Karma and Gulp

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+function defaultTask(cb) {
+    // place code for your default task here
+    cb();
+  }
+  
+  exports.default = defaultTask
+
+  var gulp = require('gulp');
+var Server = require('karma').Server;
+
+/**
+ * Run test once and exit
+ */
+gulp.task('test', function (done) {
+  new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done).start();
+});
+
+/**
+ * Watch for file changes and re-run tests on each change
+ */
+gulp.task('tdd', function (done) {
+    new Server({
+      configFile: __dirname + '/karma.conf.js'
+    }, done).start();
+  });
+  
+  gulp.task('default', ['tdd']);
